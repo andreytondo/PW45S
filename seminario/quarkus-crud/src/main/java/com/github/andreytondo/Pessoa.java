e82d06960a90aadffd38554ee84fb7c5ff3265d6
andreytondo <andreytondo0408@gmail.com>
2025-05-05 19:17:45 -0300

    seminario

--- seminario/quarkus-crud/src/main/java/com/github/andreytondo/Pessoa.java
+++ seminario/quarkus-crud/src/main/java/com/github/andreytondo/Pessoa.java
@@ -2,8 +2,20 @@ package com.github.andreytondo;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter @Setter
 public class Pessoa extends PanacheEntity {
     public String nome;
+    public String cpf;
+    public String telefone;
+    public String rua;
+    public String numero;
+    public String complemento;
+    public String bairro;
+    public String cep;
+    public String cidade;
+    public String estado;
 }
--- seminario/quarkus-crud/src/main/java/com/github/andreytondo/PessoaRepository.java
+++ seminario/quarkus-crud/src/main/java/com/github/andreytondo/PessoaRepository.java
@@ -2,10 +2,6 @@ package com.github.andreytondo;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.RequestScoped;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
 
 @RequestScoped
 public class PessoaRepository implements PanacheRepository<Pessoa> {
