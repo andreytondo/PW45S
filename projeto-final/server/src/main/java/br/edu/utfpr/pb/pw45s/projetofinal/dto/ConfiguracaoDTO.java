03288dc08aec6b6c7924d4bf80fc3f6688602190
andreytondo <andreytondo0408@gmail.com>
2025-06-30 18:51:38 -0300

    feat: adicionar DTOs

--- projeto-final/server/src/main/java/br/edu/utfpr/pb/pw45s/projetofinal/dto/ConfiguracaoDTO.java
+++ projeto-final/server/src/main/java/br/edu/utfpr/pb/pw45s/projetofinal/dto/ConfiguracaoDTO.java
@@ -1,4 +1,14 @@
 package br.edu.utfpr.pb.pw45s.projetofinal.dto;
 
+import java.time.LocalDateTime;
+
 public class ConfiguracaoDTO {
+    private Long id;
+    private String nome;
+    private String email;
+    private String pais;
+    private String idioma;
+    private String cidadeEestado;
+    private LocalDateTime dataCriacao;
+    private LocalDateTime dataAtualizacao;
 }
