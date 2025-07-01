c1ee3024c1f9a1a9d6dda036f756cbfc2d9be9ed
andreytondo <andreytondo0408@gmail.com>
2025-06-30 22:24:52 -0300

    feat: layer calc

--- projeto-final/server/src/main/java/br/edu/utfpr/pb/pw45s/projetofinal/controller/ScoreController.java
+++ projeto-final/server/src/main/java/br/edu/utfpr/pb/pw45s/projetofinal/controller/ScoreController.java
@@ -1,20 +1,18 @@
 package br.edu.utfpr.pb.pw45s.projetofinal.controller;
 
-import br.edu.utfpr.pb.pw45s.projetofinal.model.Camada;
-import br.edu.utfpr.pb.pw45s.projetofinal.utils.LayerData;
 import br.edu.utfpr.pb.pw45s.projetofinal.utils.ScoreCalculator;
+import br.edu.utfpr.pb.pw45s.projetofinal.utils.ScoreRequest;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("score")
 public class ScoreController {
 
     @PutMapping("block")
-    public double getBlockScore(List<LayerData> camadas) {
-        return ScoreCalculator.calcular(camadas);
+    public double getBlockScore(@RequestBody ScoreRequest request) {
+        return ScoreCalculator.calcular(request.getCamadas());
     }
 }
