be9407351fef15b33403bf203bfcd51e289883c2
andreytondo <andreytondo0408@gmail.com>
2025-06-30 22:02:54 -0300

    feat: minIO e cálculo de score

--- projeto-final/server/src/main/java/br/edu/utfpr/pb/pw45s/projetofinal/controller/ScoreController.java
+++ projeto-final/server/src/main/java/br/edu/utfpr/pb/pw45s/projetofinal/controller/ScoreController.java
@@ -1,6 +1,7 @@
 package br.edu.utfpr.pb.pw45s.projetofinal.controller;
 
 import br.edu.utfpr.pb.pw45s.projetofinal.model.Camada;
+import br.edu.utfpr.pb.pw45s.projetofinal.utils.LayerData;
 import br.edu.utfpr.pb.pw45s.projetofinal.utils.ScoreCalculator;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +14,7 @@ import java.util.List;
 public class ScoreController {
 
     @PutMapping("block")
-    public double getBlockScore(List<Camada> camadas) {
+    public double getBlockScore(List<LayerData> camadas) {
         return ScoreCalculator.calcular(camadas);
     }
 }
