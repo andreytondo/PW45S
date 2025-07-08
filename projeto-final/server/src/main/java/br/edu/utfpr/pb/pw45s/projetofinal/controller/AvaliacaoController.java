9497e4fe437d20694029e3eb4ea5df8de4d27b9d
Rafaela Carbelim Bandeira <rafaelacarbelimbandeira@gmail.com>
2025-07-07 21:10:32 -0300

    avaliacao

--- projeto-final/server/src/main/java/br/edu/utfpr/pb/pw45s/projetofinal/controller/AvaliacaoController.java
+++ projeto-final/server/src/main/java/br/edu/utfpr/pb/pw45s/projetofinal/controller/AvaliacaoController.java
@@ -5,8 +5,13 @@ import br.edu.utfpr.pb.pw45s.projetofinal.model.Avaliacao;
 import br.edu.utfpr.pb.pw45s.projetofinal.repository.AvaliacaoRepository;
 import br.edu.utfpr.pb.pw45s.projetofinal.service.AvaliacaoService;
 import br.edu.utfpr.pb.pw45s.projetofinal.shared.CrudController;
+import org.simpleframework.xml.Path;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("avaliacao")
@@ -14,6 +19,11 @@ public class AvaliacaoController extends CrudController<Long, Avaliacao, Avaliac
 
     public AvaliacaoController() {
         super(Avaliacao.class, AvaliacaoDTO.class);
+    }
+
+    @GetMapping("/all")
+    public List<AvaliacaoDTO> findAll() {
+        return service.findAll().stream().map(this::toDto).collect(Collectors.toList());
     }
 
 }
--- projeto-final/server/src/main/java/br/edu/utfpr/pb/pw45s/projetofinal/service/AvaliacaoService.java
+++ projeto-final/server/src/main/java/br/edu/utfpr/pb/pw45s/projetofinal/service/AvaliacaoService.java
@@ -5,6 +5,12 @@ import br.edu.utfpr.pb.pw45s.projetofinal.repository.AvaliacaoRepository;
 import br.edu.utfpr.pb.pw45s.projetofinal.shared.CrudService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class AvaliacaoService extends CrudService<Long, Avaliacao, AvaliacaoRepository> {
+
+    public List<Avaliacao> findAll() {
+        return repository.findAll();
+    }
 }
